2c588ac7f60a4c7af55c368d506c0ce1a6b4e8f7
TimeGo Dev <timego.market@gmail.com>
2024-10-16 23:51:29 +0700

    Add controlelr

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/history')
+  saveAddress(@Body() body) {
+    return this.appService.addAddress(body);
+  }
+
+  @Get('/history')
+  getAddress() {
+    return this.appService.getAddress();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async addAddress(data: any) {
+    return data;
+  }
+
+  async getAddress() {
+    return [];
+  }
 }
